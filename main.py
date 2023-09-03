d0904e56b6e8fd922587ab489455746b9d34eb21
Zekun Li <zli909@gatech.edu>
2023-09-03 18:53:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from pathlib import Path
 
 order_file_path = "test.csv"
 dues_book_path = "dues.xlsx"
-dues_book = xl.load_workbook(dues_book_path) if Path(dues_book_path).is_file() else xl.Workbook()
-
+dues_book_exists = True if Path(dues_book_path).is_file() else False
+dues_book = xl.load_workbook(dues_book_path) if dues_book_exists else xl.Workbook()
 orders = {}
+
+# if not dues_book_exists:
+#     dues_book.create_sheet(title="Alumni")
+#     dues_book.create_sheet(title="Grad")
+#     dues_book.create_sheet(title="Undergrad")
+#     dues_book.create_sheet(title="Staff")
 
 with open(order_file_path, "r", encoding="utf-8") as order_file:
     order_dict = csv.DictReader(order_file)
